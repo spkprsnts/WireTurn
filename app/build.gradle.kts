a834afe52c1586f8721356725f923055142521e6
spkprsnts <jectokuu@gmail.com>
2026-04-22 00:06:44 +0500

    chore: bump версии до 2.3.3 (37)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.wireturn.app"
         minSdk = project.property("project.minSdk").toString().toInt()
         targetSdk = project.property("project.targetSdk").toString().toInt()
-        versionCode = 36
-        versionName = "2.3"
+        versionCode = 37
+        versionName = "2.3.3"
     }
 
     splits {
